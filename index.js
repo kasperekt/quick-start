3c98503dca41440c166f2284ce073a0506133670
Tomasz Kasperek <kasperekt@gmail.com>
2015-08-30 19:31:48 +0200

    Removing projects

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var path = require('path');
 var fs = require('fs');
+var wrench = require('wrench');
 var argv = require('minimist')(process.argv.slice(2), {
   boolean: [
     'c', 'create',
-    's', 'scan'
+    's', 'scan',
+    'd', 'delete'
   ]
 });
-var wrench = require('wrench');
 
 var exclude = ['node_modules', '.git', 'bower_components', 'npm-debug.log'];
 
@@ -52,15 +53,49 @@ function createProject(name, dest) {
   }
 }
 
-function scanProject(name, dest) {
+function scanProject(name, src) {
   var projectPath = getProjectPath(name);
 
   if (_projectExists(projectPath)) {
     console.log('Project already exists');
     process.exit(0);
   }
-  
-  console.log('Scan -- name: %s, dest: %s', name, dest);
+
+  try {
+    wrench.copyDirSyncRecursive(
+      src,
+      projectPath,
+      {
+        whitelist: true,
+        exclude: function(filename, dir) {
+          return exclude.some(function(filter) {
+            return filter === filename;
+          });
+        }
+      }
+    );  
+
+    console.log('Successfully scanned ' + name + ' project!');
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+function removeProject(name) {
+  var projectPath = getProjectPath(name);
+  if (!_projectExists(projectPath)) {
+    console.error(name + ' project doesn\'t exist!');
+    process.exit(1);
+  }
+
+  try {
+    wrench.rmdirSyncRecursive(projectPath);
+    console.log('Successfully removed ' + name + ' project!');
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
 
 function printHelp() {
@@ -73,4 +108,5 @@ function printHelp() {
 
 if (argv.c || argv.create) createProject.apply(null, argv._);
 else if (argv.s || argv.scan) scanProject.apply(null, argv._);
+else if (argv.d || argv.delete) removeProject.apply(null, argv._);
 else printHelp();
