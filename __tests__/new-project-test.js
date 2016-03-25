51670a35dfdee78dfea0397e6f90dfc18868f6e8
Tomasz Kasperek <kasperekt@gmail.com>
2016-03-25 23:00:14 +0100

    (test) new project creation tests

--- __tests__/new-project-test.js
+++ __tests__/new-project-test.js
@@ -10,11 +10,29 @@ function dirExists(dir) {
   return stat.isDirectory();
 }
 
+function fileExists(file) {
+  const stat = fs.statSync(file);
+  return stat.isFile();
+}
+
 describe('Main functions test', () => {
+  beforeEach(() => {
+    // Silence all console logs
+    console.log = () => {};
+  });
+
   it('should create new project', () => {
     const destination = path.resolve(TEST_ENV_DIR, 'newProjectTest');
     newProject('project-with-config', destination);
 
     expect(dirExists(destination)).toBe(true);
+  });
+
+  it('should copy files', () => {
+    const destination = path.resolve(TEST_ENV_DIR, 'copyFilesTest');
+    newProject('project-with-file', destination);
+
+    const filePath = path.resolve(destination, 'index.html');
+    expect(fileExists(filePath)).toBe(true);
   });
 });
--- scripts/pretest.sh
+++ scripts/pretest.sh
@@ -2,6 +2,7 @@ PROJECTS_DIR="test_env/.quickstart/projects"
 
 if [ ! -d "test_env" ]; then
   mkdir -p $PROJECTS_DIR
-  mkdir $PROJECTS_DIR/project-with-config $PROJECTS_DIR/project-without-config
+  mkdir $PROJECTS_DIR/project-with-config $PROJECTS_DIR/project-without-config $PROJECTS_DIR/project-with-file
   printf '{\n\n}\n' > $PROJECTS_DIR/project-with-config/.quickstartrc
+  printf '<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body>text</body>\n</html>\n' > $PROJECTS_DIR/project-with-file/index.html
 fi
