01be74c7ded5d075acf7cc26686213bd0d9e4271
Tomasz Kasperek <kasperekt@gmail.com>
2016-04-23 18:16:52 +0200

    (test) existing project scan test

--- __tests__/scan-project-test.js
+++ __tests__/scan-project-test.js
@@ -6,21 +6,22 @@ import { dirExists } from './test-utils';
 import { TEST_ENV_DIR } from './test-constants';
 
 describe('Scan project', () => {
-  let consoleLog;
-  let consoleError;
+  const consoleLog = console.log;
+  const consoleError = console.error;
+  const processExit = process.exit;
 
   beforeAll(() => {
     // Silence console methods
-    consoleLog = console.log;
     console.log = () => {};
-    consoleError = console.error;
     console.error = () => {};
+    process.exit = jest.fn();
   });
 
   afterAll(() => {
     // Recover console methods
     console.log = consoleLog;
     console.error = consoleError;
+    process.exit = processExit;
   });
 
   it('should save scanned project in right place', () => {
@@ -35,5 +36,52 @@ describe('Scan project', () => {
     );
 
     expect(dirExists(scannedProjectPath)).toBe(true);
+  });
+
+  it('should not save ignored files in project dir', () => {
+    const scannedProjectName = 'scanned-project-with-ignore';
+    const projectSource = path.resolve(
+      TEST_ENV_DIR,
+      'projects-to-scan',
+      'project-with-ignore'
+    );
+    scanProject(scannedProjectName, projectSource);
+    const scannedProjectPath = path.resolve(
+      TEST_ENV_DIR,
+      '.quickstart',
+      'projects',
+      scannedProjectName
+    );
+
+    expect(dirExists(scannedProjectPath)).toBe(true);
+  });
+
+  it('should let scan directory without config file', () => {
+    const scannedProjectName = 'scanned-project-without-config';
+    const projectSource = path.resolve(
+      TEST_ENV_DIR,
+      'projects-to-scan',
+      'project-without-config'
+    );
+    scanProject(scannedProjectName, projectSource);
+    const scannedProjectPath = path.resolve(
+      TEST_ENV_DIR,
+      '.quickstart',
+      'projects',
+      scannedProjectName
+    );
+
+    expect(dirExists(scannedProjectPath)).toBe(true);
+  });
+
+  it('should not allow to scan project with existing name', () => {
+    const existingProjectName = 'project-with-config';
+    const projectSource = path.resolve(
+      TEST_ENV_DIR,
+      'projects-to-scan',
+      'existing-project'
+    );
+    scanProject(existingProjectName, projectSource);
+    expect(process.exit).toBeCalled();
   });
 });
